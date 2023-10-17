e19b597e937d2b89c03faa61dd1e654fa448d705
tuanphi1995 <145833059+tuanphi1995@users.noreply.github.com>
2023-10-17 15:19:47 +0700

    up1

---
+++ js/slider.js
@@ -0,0 +1,54 @@
+$('.slider').each(function(){
+    var $this = $(this);
+    var $group = $this.find('.slide-group');
+    var $slides = $this.find('.slide');
+    var buttonArray = [];
+    var currentIndex = 0;
+    var timeout;
+    function move(newIndex){
+        var animateLeft, sideLeft;
+
+        advance();
+        if($group.is(':animated')|| currentIndex === newIndex){
+            return;
+        }
+        buttonArray[currentIndex].removeClass('active');
+        buttonArray[newIndex].addClass('active');
+        if(newIndex>currentIndex){
+            sideLeft='100%';
+            animateLeft='-100%';
+        }else{
+            sideLeft='-100%';
+            animateLeft='100%'
+        }
+        $slides.eq(newIndex).css({left: sideLeft, display: 'block'});
+        $group.animate({left: sideLeft}, function(){
+            $slides.eq(currentIndex).css({display:'none'});
+            $slides.eq(newIndex).css({left:0});
+            $group.css({left:0});
+            currentIndex=newIndex;
+        });
+    }
+    function advance(){
+        clearTimeout(timeout);
+        timeout = setTimeout(function(){
+            if(currentIndex<($slides.length -1)){
+                move(currentIndex+1);
+            }else{
+                move(0);
+            }
+        },4000);
+    }
+    $.each($slides,function(index){
+        var $button = $('<button type="button" class="slide-btn">#bull;</button>');
+        if(index===currentIndex){
+            $button.addClass('active');
+        }
+        $button.on('click', function(){
+            move(index);
+        }).appendto('.slide-buttons');
+        buttonArray.push($button);
+    });
+    advance();
+
+});
